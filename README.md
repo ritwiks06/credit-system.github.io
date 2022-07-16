9fc92f16ac6794554dfcd540534ac747abf2b9b4
ritwiks06 <107185184+ritwiks06@users.noreply.github.com>
2022-07-16 11:46:42 +0530

    credit system website

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# credit-system.github.io
+Credit Manager website
+
+This project helps to transfer credits from one user to another within the given list. Credits are a sort of points which can be transferred between users. Backend uses Django and frontend uses HTML, CSS and Bootstrap.
---
+++ manager/urls.py
@@ -0,0 +1,24 @@
+"""
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('transfer_credit', views.transfer_credit, name='transfer_credit'),
+    path('user/<str:name>', views.user, name='user'),
+    path('user/<str:name>/confirmation', views.confirmation, name='confirmation')
+]
